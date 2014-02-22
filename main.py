c762c859e829b2698e90386bc70639bf0b39a434
Ryan  Stush <Akintoc@ryans-mbp-3.wireless.rit.edu>
2014-02-22 02:34:37 -0500

    Added Alarm Functionality - still needs a song/sound Adding SoundAnalyse for future sound trigger funcitonality

--- main.py
+++ main.py
@@ -2,17 +2,42 @@
 import time
 import urllib2
 import json
+import os
+#Date-Time functionality
+#--------------------------------------------------------------------------
+_date = time.strftime("%d/%m/%Y")
+_time = time.strftime("%I:%M%p")
+print _date
+print _time
 
-
-#Print the current date and time for reference
-print time.strftime("%d/%m/%Y")
-print time.strftime("%I:%M%p")
-
-
-f = urllib2.urlopen('http://api.wunderground.com/api/your_API_key/geolookup/conditions/q/IA/Rochester.json')
+'''
+#Weather functionality - courtesy of the weather underground API
+#--------------------------------------------------------------------------
+f = urllib2.urlopen('http://api.wunderground.com/api/YOUR_API_KEY/geolookup/conditions/q/IA/Rochester.json')
 json_string = f.read()
 parsed_json = json.loads(json_string)
 location = parsed_json['location']['city']
 temp_f = parsed_json['current_observation']['temp_f']
 print "Current temperature in %s is: %s" % (location, temp_f)
 f.close()
+'''
+#Add webcam functionality
+#--------------------------------------------------------------------------
+
+#Add Alarm functionality
+#-------------------------------------------------------------------------
+alarm_HH = input("Hour: ")
+alarm_MM = input("Minute: ")
+print("We're set for", alarm_HH , alarm_MM)
+
+while True:
+    now = time.localtime()
+    if now.tm_hour == int(alarm_HH) and now.tm_min  == int(alarm_MM):
+        print("Yup this works!")
+        #os.popen("YOUR SONG / SOUND HERE")
+        break
+    else:
+        time.sleep(30)
+        print("not yet!")
+        if now.tm_hour > int(alarm_HH) and now/tm_min > int(alarm_MM):
+            break
